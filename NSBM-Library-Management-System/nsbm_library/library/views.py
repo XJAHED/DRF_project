716de1c3bbebc92a21a8c853c9847809247120d8
XJAHED <jmd009837@gmail.com>
2026-08-16 12:39:17 +0600

    email

--- NSBM-Library-Management-System/nsbm_library/library/views.py
+++ NSBM-Library-Management-System/nsbm_library/library/views.py
@@ -6,6 +6,8 @@ from .models import Author, Category, Books
 from .serializers import AuthorSerializer, CategorySerializer, BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from django.db import connections
+import threading
 # Create your views here.
 
 
@@ -18,7 +20,14 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [AllowAny]
-    
+
+
+def send_book_created(book_number, book_name):
+    try:
+        for book in Books.objects.all():
+            print(book.book_number, book.book_name)
+    finally:
+        connections.close_all()
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BookSerializer
@@ -26,4 +35,12 @@ class BookViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['category']
     search_fields = ['book_name']
+    def perform_create(self, serializer):
+        isinstance = serializer.save()
+        thread = threading.Thread(
+            target=send_book_created,
+            args=(isinstance.book_number, isinstance.book_name)
+        )
+        thread.daemon = True
+        thread.start()
     
